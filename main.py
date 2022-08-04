fd9318772da33becac7ea92a2d61f371e222b520
Usagi no Niku <chenbohan911@163.com>
2022-08-04 16:41:27 +0000

    bump version & enter key when beatmaps search

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ from objects import glob
 
 app = Quart(__name__)
 
-version = Version(1, 3, 3)
+version = Version(1, 3, 4)
 
 # used to secure session data.
 # we recommend using a long randomly generated ascii string.
