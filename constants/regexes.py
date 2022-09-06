bcd4fd9c66f0abccccbcf0ed2f2009514213e401
nekoraw <michelbarros123456@gmail.com>
2022-09-06 10:33:45 -0300

    fix regex

--- constants/regexes.py
+++ constants/regexes.py
@@ -4,4 +4,4 @@ import re
 
 username = re.compile(r'^[\w \[\]-]{2,15}$')
 email = re.compile(r'^[^@\s]{1,200}@[^@\s\.]{1,30}\.[^@\.\s]{1,24}$')
-key = re.compile(r"[0-9a-f]{12}4[0-9a-f]{3}[89ab][0-9a-f]{15}\Z")
+key = re.compile(r"^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$\Z")
