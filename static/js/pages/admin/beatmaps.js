04b40bae24afcee1106be57020293a3520d995a6
Usagi no Niku <chenbohan911@163.com>
2022-12-26 01:12:28 +0000

    beatmap force update

--- static/js/pages/admin/beatmaps.js
+++ static/js/pages/admin/beatmaps.js
@@ -5,3 +5,33 @@ function searchMaps () {
   if (search.value === '') window.location.href = '/admin/beatmaps'
   else window.location.href = '/admin/beatmaps/search/' + search.value
 }
+
+function updateMaps(sid) {
+  _testGlobals(
+      { exists: ['domain', 'api_key'] }
+  )
+  var bar = document.getElementById('progress_bar')
+  var elements = document.getElementsByClassName('operate-button');
+  bar && bar.removeAttribute('style')
+  Array.prototype.forEach.call(elements, function (element) {
+      element.classList.add('is-disabled')
+  });
+  $.ajax({
+    type: "GET",
+    url: '//api.' + window.domain + '/update_maps?&sid=' + sid,
+    beforeSend: function(request) {
+        request.setRequestHeader("Authorization", window.api_key);
+    },
+    success: function (data, status) {
+      bar && setStyle(bar, {
+          display: 'none'
+      })
+      Array.prototype.forEach.call(elements, function (element) {
+          element.classList.remove('is-disabled')
+      });
+      if (status == 'success') {
+          window.location.href = '/admin/beatmaps/search/' + data['sid']
+      }
+  }
+});
+}
