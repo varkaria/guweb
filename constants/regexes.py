5a5b1445fb9a2e3d8c8ff3fd19b98e3594809767
nekoraw <michelbarros123456@gmail.com>
2023-06-09 14:50:16 +0000

    fix page

--- constants/regexes.py
+++ constants/regexes.py
@@ -4,4 +4,4 @@ import re
 
 username = re.compile(r'^[\w \[\]-]{2,15}$')
 email = re.compile(r'^[^@\s]{1,200}@[^@\s\.]{1,30}\.[^@\.\s]{1,24}$')
-key = re.compile(r"[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}")
+key = re.compile(r"[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}", re.I)
